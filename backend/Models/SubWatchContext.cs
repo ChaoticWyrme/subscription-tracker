6d1268ea635b620e0de02b15d69b60012b0364f5
Mason Osborn <mason.osborn@gmail.com>
2021-01-07 01:28:23 -0500

    Initial backend work

---
+++ backend/Models/SubWatchContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SubWatchApi.Models
+{
+    public class SubWatchContext : DbContext
+    {
+        public SubWatchContext(DbContextOptions<SubWatchContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ServiceInfo> Services { get; set; }
+
+        public DbSet<TimeData> TimeDatas { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<TimeData>().Property<>
+        //}
+    }
+}
