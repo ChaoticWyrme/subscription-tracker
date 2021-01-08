be4ed53155785d7fbc86328b1de487a4829b7606
Victor Yang <vyang1222@gmail.com>
2021-01-07 23:46:58 -0600

    more work

--- combine/frontend/js/presetsscript.js
+++ combine/frontend/js/presetsscript.js
@@ -1,80 +1,79 @@
-console.log("hello1");
-
 const subscriptionpresets = [
-    {
-        "id" : "netflix1",
-        "description" : "Netflix Basic",
-        "url" : "netflix.com",
-        "price" : 8.99,
-        "icon" : "images/netflixicon.png"
-    },
-    {
-        "id" : "netflix2",
-        "description" : "Netflix Standard",
-        "url" : "netflix.com",
-        "price" : 13.99,
-        "icon" : "images/netflixicon.png"
-    },
-    {
-        "id" : "netflix3",
-        "description" : "Netflix Premium",
-        "url" : "netflix.com",
-        "price" : 17.99,
-        "icon" : "images/netflixicon.png"
-    },
-    {
-        "id" : "hulu",
-        "description" : "Hulu",
-        "url" : "hulu.com",
-        "price" : 5.99,
-        "icon" : "images/huluicon.png"
-    },
-    {
-        "id" : "hbomax",
-        "description" : "HBO Max",
-        "url" : "hbomax.com",
-        "price" : 14.99,
-        "icon" : "images/hboicon.png"
-    },
-    {
-        "id" : "amazonprime",
-        "description" : "Amazon Prime Video",
-        "url" : "amazon.com",
-        "price" : 8.99,
-        "icon" : "images/amazonprimeicon.png"
-    },
-    {
-        "id" : "disneyplus",
-        "description" : "Disney Plus",
-        "url" : "disneyplus",
-        "price" : 6.99,
-        "icon" : "images/disneyplusicon.png"
-    },
-    {
-        "id" : "starz",
-        "description" : "Starz",
-        "url" : "starz.com",
-        "price" : 8.99,
-        "icon" : "images/starzicon.png"
-    },
-    {
-        "id" : "mubi",
-        "description" : "Mubi",
-        "url" : "mubi.com",
-        "price" : 10.99,
-        "icon" : "images/mubiicon.png"
-    }
-]
+  {
+    id: "netflix1",
+    description: "Netflix Basic",
+    url: "netflix.com",
+    price: 8.99,
+    icon: "images/netflixicon.png",
+  },
+  {
+    id: "netflix2",
+    description: "Netflix Standard",
+    url: "netflix.com",
+    price: 13.99,
+    icon: "images/netflixicon.png",
+  },
+  {
+    id: "netflix3",
+    description: "Netflix Premium",
+    url: "netflix.com",
+    price: 17.99,
+    icon: "images/netflixicon.png",
+  },
+  {
+    id: "hulu",
+    description: "Hulu",
+    url: "hulu.com",
+    price: 5.99,
+    icon: "images/huluicon.png",
+  },
+  {
+    id: "hbomax",
+    description: "HBO Max",
+    url: "hbomax.com",
+    price: 14.99,
+    icon: "images/hboicon.png",
+  },
+  {
+    id: "amazonprime",
+    description: "Amazon Prime Video",
+    url: "amazon.com",
+    price: 8.99,
+    icon: "images/amazonprimeicon.png",
+  },
+  {
+    id: "disneyplus",
+    description: "Disney Plus",
+    url: "disneyplus",
+    price: 6.99,
+    icon: "images/disneyplusicon.png",
+  },
+  {
+    id: "starz",
+    description: "Starz",
+    url: "starz.com",
+    price: 8.99,
+    icon: "images/starzicon.png",
+  },
+  {
+    id: "mubi",
+    description: "Mubi",
+    url: "mubi.com",
+    price: 10.99,
+    icon: "images/mubiicon.png",
+  },
+];
 
 //fill preseticons
-console.log("hello");
-function fillpreseticons(presetid){
-    console.log(presetid);
-    for(const service of subscriptionpresets){
-        console.log(service);
-        document.getElementById(presetid).innerHTML += `<a href="#" id=${service.id}><div class="iconbox"><img src="${service.icon}" alt=""></div>${service.description}, $${service.price}</a>`
-    }
-    document.getElementById(presetid).innerHTML += `<a href="add.html"><div class="iconbox"><img src="icon.png" alt=""></div>Other</a>`
+function fillpreseticons(presetid) {
+  for (const service of subscriptionpresets) {
+    document.getElementById(
+      presetid
+    ).innerHTML += `<a href="#" id=${service.id}><div class="iconbox"><img src="${service.icon}" alt=""></div>${service.description}, $${service.price}</a>`;
+  }
+  document.getElementById(
+    presetid
+  ).innerHTML += `<a href="add.html"><div class="iconbox"><img src="icon.png" alt=""></div>Other</a>`;
 }
 
 fillpreseticons("preseticons");
--- combine/frontend/js/script.js
+++ combine/frontend/js/script.js
@@ -1,25 +1,47 @@
-document.querySelector('.add_subscription_form').addEventListener("submit", e => {
+document.querySelector(".add_subscription_form").addEventListener("submit", async (e) => {
   //make post request
-  e.preventDefault()
+  e.preventDefault();
 
-  const data = JSON.stringify({
-    name: document.getElementById('sub_name').value,
-    price: document.getElementById('price').value,
-    url: document.getElementById('sub_url').value,
-  })
+  let storage = await browser.storage.local.get(["subList"]);
 
-  const xhr = new XMLHttpRequest()
-  xhr.open("POST", "http://chaoticwyrme-001-site1.itempurl.com/")
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === xhr.DONE) {
-      window.location.href = "home.html"
-    }
-  }
-  xhr.send(data)
-})
+  let subList = storage["subList"] || [];
 
-// function DoSubmit(){
-//   //post request to the api
-//   window.location.href = "home.html"
-//   return true;
-// }
+  let name = document.getElementById("sub_name").value;
+  let price = document.getElementById("price").value;
+  let url = document.getElementById("sub_url").value;
+
+  subList.push({
+    name,
+    price,
+    url,
+  });
+
+  console.log(subList);
+
+  // const xhr = new XMLHttpRequest();
+  // xhr.open("POST", "http://chaoticwyrme-001-site1.itempurl.com/");
+  // xhr.onreadystatechange = function () {
+  //   if (xhr.readyState === xhr.DONE) {
+  //     window.location.href = "home.html";
+  //   }
+  // };
+  // xhr.send(data);
+
+  window.location.href = "home.html";
+
+  document.getElementById("subscriptions").innerHTML += `<div id="subscriptions">
+              <div class="subscriptionbox">
+                  <div class="subscriptionleft">
+                      <img src="icon.png" class="subscriptionicon" width="60" height="60">
+                      <p class="subscriptionname">${name}</p>
+                  </div>
+                  <div class="subscriptionright">
+                      <p class="subscriptionprice">$${price}</p>
+                      <a href="#"><img src="images/graph.png" class="graphIcon" width="30" height="30"></a>
+                      <button type="button" class="close" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                      </button>
+                  </div>
+              </div>
+            </div>`;
+});
